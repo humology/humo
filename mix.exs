35b109227a809cbb546650541b2140ca2623d043
Kamil Shersheyev <kamil.shersheyev@gmail.com>
2022-05-06 14:49:24 +0200

    include config/plugin.exs to compiled package

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Humo.MixProject do
   def project do
     [
       app: :humo,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
@@ -17,7 +17,7 @@ defmodule Humo.MixProject do
         maintainers: ["Kamil Shersheyev"],
         licenses: ["Apache-2.0"],
         links: %{"GitHub" => @scm_url},
-        files: ~w(assets lib priv mix.exs package.json LICENSE README.md .formatter.exs)
+        files: ~w(assets config/plugin.exs lib priv mix.exs package.json LICENSE README.md .formatter.exs)
       ],
       source_url: @scm_url,
       humo_plugin: true,
