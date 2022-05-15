8bd84cd131aac9ee10260a693509a10728247922
Kamil Shersheyev <kamil.shersheyev@gmail.com>
2022-05-16 00:43:29 +0200

    create PluginRouterBehaviour which has default implementations of root and dashboard routers

--- lib/humo_web/plugin_router.ex
+++ lib/humo_web/plugin_router.ex
@@ -1,21 +1,11 @@
 defmodule HumoWeb.PluginRouter do
   @moduledoc false
 
-  def root() do
-    quote location: :keep do
-    end
-  end
+  use HumoWeb.PluginRouterBehaviour
 
   def dashboard() do
     quote location: :keep do
       get "/", HumoWeb.Dashboard.PageController, :index
     end
-  end
-
-  @doc """
-  When used, dispatch to the appropriate router.
-  """
-  defmacro __using__(which) when is_atom(which) do
-    apply(__MODULE__, which, [])
   end
 end
---
+++ lib/humo_web/plugin_router_behaviour.ex
@@ -0,0 +1,48 @@
+defmodule HumoWeb.PluginRouterBehaviour do
+  @moduledoc false
+
+  defmacro __using__(_opts) do
+    quote do
+      @behaviour HumoWeb.PluginRouterBehaviour
+
+      @doc """
+      Root routes
+      """
+      @impl HumoWeb.PluginRouterBehaviour
+      @spec root() :: Macro.t()
+      def root() do
+        quote location: :keep do
+        end
+      end
+
+      @doc """
+      Dashboard routes
+      """
+      @impl HumoWeb.PluginRouterBehaviour
+      @spec dashboard() :: Macro.t()
+      def dashboard() do
+        quote location: :keep do
+        end
+      end
+
+      @doc """
+      When used, dispatch to the appropriate router.
+      """
+      defmacro __using__(which) when is_atom(which) do
+        apply(__MODULE__, which, [])
+      end
+
+      defoverridable root: 0, dashboard: 0
+    end
+  end
+
+  @doc """
+  Root routes
+  """
+  @callback root() :: Macro.t()
+
+  @doc """
+  Dashboard routes
+  """
+  @callback dashboard() :: Macro.t()
+end
